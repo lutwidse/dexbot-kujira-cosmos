706ce88d41fb9905c3d50727f1e5859711b2357c
Lutwidse <lutwidse@m0n4.com>
2023-01-16 15:08:28 +0900

    change use wrapper instead of internal library

--- liquidation_bot.ts
+++ liquidation_bot.ts
@@ -1,12 +1,16 @@
-import { kns, registry, } from "kujira.js";
-import { GasPrice, coins} from "@cosmjs/stargate";
+import { registry, } from "kujira.js";
+import { GasPrice} from "@cosmjs/stargate";
 import { SigningCosmWasmClient } from "@cosmjs/cosmwasm-stargate";
 import { DirectSecp256k1HdWallet } from "@cosmjs/proto-signing";
+import { MsgExecuteContract } from "cosmjs-types/cosmwasm/wasm/v1/tx";
+const { toUtf8 } = require("@cosmjs/encoding");
+import "kujira_wrapper.js"
 
 const RPC_ENDPOINT = "https://rpc-harpoon.kujira.app";
 const MNEMONIC = "...";
 
 const signer = await DirectSecp256k1HdWallet.fromMnemonic(MNEMONIC);
+const [{ address: signerAddress }] = await signer.getAccounts();
 
 const client = await SigningCosmWasmClient.connectWithSigner(
   RPC_ENDPOINT,
@@ -17,14 +21,32 @@ const client = await SigningCosmWasmClient.connectWithSigner(
   }
 );
 
-const [account] = await signer.getAccounts();
+const ATOM_USK_CONTRACT = "kujira1yum4v0v5l92jkxn8xpn9mjg7wuldk784ctg424ue8gqvdp88qzlqr2qp2j"
 
-// const ATOM_USK_LP = "kujira1yncutssgh2vj9scaymtteg949hwcft07c6qmgarxnaf04yesq3jsn6g2uv"
-// const ATOM_USK_VAULT = "kujira1d5v9z4xzfswjh80r0ynuup68qzkr45lktje83gdxlnmnfnh4wvys3zr8pj"
-const ATOM_USK = "kujira1yum4v0v5l92jkxn8xpn9mjg7wuldk784ctg424ue8gqvdp88qzlqr2qp2j"
+client.signAndBroadcast(signerAddress,[{
+    typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+    value: MsgExecuteContract.fromPartial({
+        sender: signerAddress,
+        contract: ATOM_USK_CONTRACT,
+        // 1USK $10.20 per ATOM
+        msg: toUtf8(JSON.stringify(msg_submit_order("1","10.20"))
+        ),
+    },
+    )
+}], "auto")
 
-// contractAddressが何を指すのか不明 - 暫定
-const auctionClient = new kns.auctions.AuctionsClient(client, account.address, ATOM_USK)
+/*
+ここで何かしらの処理をしてorder_idxsを取得する
+submit_orderの返り値からも取得できるが、他にも便利な方法があるかもしれないのでひとまず空にしておく
+*/
 
-// domainが何を指すのか不明 - 暫定
-auctionClient.placeBid({"domain":"ATOM"}, "auto", "", coins(1000000, "uusk"))
+client.signAndBroadcast(signerAddress,[{
+    typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+    value: MsgExecuteContract.fromPartial({
+        sender: signerAddress,
+        contract: ATOM_USK_CONTRACT,
+        msg: toUtf8(JSON.stringify(msg_withdraw_order(["12345","67890"]))
+        ),
+    },
+    )
+}], "auto")
