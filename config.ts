54b126755ebd6947b6f5175bc2454e470636fc7e
Lutwidse <lutwidse@m0n4.com>
2023-01-17 15:20:18 +0900

    add config.ts

---
+++ config.ts
@@ -0,0 +1,14 @@
+export const RPC_ENDPOINT = 'https://rpc.kaiyo.kujira.setten.io';
+export const MNEMONIC = '...';
+
+export const DENOM_AMOUNT = 1000000;
+
+export const ATOM_USK_CONTRACT =
+  'kujira1yum4v0v5l92jkxn8xpn9mjg7wuldk784ctg424ue8gqvdp88qzlqr2qp2j';
+export const ATOM_DENOM =
+  'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2';
+
+export const ORCA_MARKET_USK_ATOM_CONTRACT =
+  'kujira1q8y46xg993cqg3xjycyw2334tepey7dmnh5jk2psutrz3fc69teskctgfc';
+export const KUJI_DENOM =
+  'factory/kujira1qk00h5atutpsv900x202pxx42npjr9thg58dnqpa72f2p7m2luase444a7/uusk';
