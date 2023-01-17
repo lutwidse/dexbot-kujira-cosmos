e360bea7889524dcbbe5b1d9aae00af876459012
Lutwidse <lutwidse@m0n4.com>
2023-01-17 15:25:13 +0900

    add prettier

--- kujira_wrapper.ts
+++ kujira_wrapper.ts
@@ -1,40 +1,40 @@
 // FIN
-export function msg_submit_order(uusk_amount: string, collateral_liquidation_price: string) {
-    return {
-        "submit_order": {
-          "amount": (parseInt(uusk_amount)*1000000).toString(),
-          "price": collateral_liquidation_price.toString(),
-          "denom": {
-            "native": "factory/kujira1qk00h5atutpsv900x202pxx42npjr9thg58dnqpa72f2p7m2luase444a7/uusk"
-          }
-        }
+export function msg_submit_order(
+  uusk_amount: string,
+  collateral_liquidation_price: string
+) {
+  return {
+    submit_order: {
+      amount: (parseInt(uusk_amount) * 1000000).toString(),
+      price: collateral_liquidation_price.toString(),
+      denom: {
+        native:
+          'factory/kujira1qk00h5atutpsv900x202pxx42npjr9thg58dnqpa72f2p7m2luase444a7/uusk'
+      }
     }
+  };
 }
 
-export function msg_withdraw_order(order_idxs:string[]) {
-    return {
-        "withdraw_orders": {
-          "order_idxs": [
-            order_idxs
-          ]
-        }
+export function msg_withdraw_order(order_idxs: string[]) {
+  return {
+    withdraw_orders: {
+      order_idxs: [order_idxs]
     }
+  };
 }
 
 export function msg_swap() {
-    return {
-        "swap":{}
-    }
-}
-
-export function msg_submit_bid(premium: number) {
-    return {
-        "submit_bid": {
-            "premium_slot": premium,
-            "delegate": "kujira16a03hk5ev6963a4yj3kcrvmh4hej3w3j70kv2n"
-        }
-    }
+  return {
+    swap: {}
+  };
 }
 
 // Orca
-// ...
+export function msg_submit_bid(premium: number) {
+  return {
+    submit_bid: {
+      premium_slot: premium,
+      delegate: 'kujira16a03hk5ev6963a4yj3kcrvmh4hej3w3j70kv2n'
+    }
+  };
+}
