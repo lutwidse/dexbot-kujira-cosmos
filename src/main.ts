a1406e68c3396a321edcae0998504139a726adfa
Lutwidse <lutwidse@m0n4.com>
2023-01-19 00:34:55 +0900

    fix main algorithm

--- src/main.ts
+++ src/main.ts
@@ -12,30 +12,27 @@ bot.then(function (b) {
     while (true) {
       // Bidの確認
       const bids = await b.getBids(false);
-      if (bids.length > BID_MAX) {
-        break;
-        // Bidが存在していないなら新たに発行
-      } else {
+      // Bidの注文数が最大に達していないなら新規新規注文を発行
+      if (bids.length <= BID_MAX) {
         // USKの残高を取得してBid
         const uskBalance = await b.getTokenBalance(USK_DENOM);
-        // USKの残高がBID_MINより下回るなら拒否
-        if (parseFloat(uskBalance) <= BID_MIN_USK) {
-          break;
+        // USKの残高がBID_MINを上回るなら新規注文の発行を継続
+        if (parseFloat(uskBalance) >= BID_MIN_USK) {
+          await b.submitBid(PREMIUM, parseFloat(uskBalance));
         }
-        await b.submitBid(PREMIUM, parseFloat(uskBalance));
-      }
 
-      // 清算済みBidの確認
-      const bidsClaimable = await b.getBids(true);
-      if (bidsClaimable.length > 0) {
-        // 清算した担保の受け取り
-        await b.claimLiquidations(bidsClaimable);
-        // 清算したATOMをUSKにスワップ
-        const atomBalance = await b.getTokenBalance(ATOM_DENOM);
-        await b.swapAtomToUsk(parseFloat(atomBalance));
-      }
+        // 清算済みBidの確認
+        const bidsClaimable = await b.getBids(true);
+        if (bidsClaimable.length > 0) {
+          // 清算した担保の受け取り
+          await b.claimLiquidations(bidsClaimable);
+          // 清算したATOMをUSKにスワップ
+          const atomBalance = await b.getTokenBalance(ATOM_DENOM);
+          await b.swapAtomToUsk(parseFloat(atomBalance));
+        }
 
-      await delay(60 * 1000);
+        await delay(60 * 1000);
+      }
     }
   })();
 });
