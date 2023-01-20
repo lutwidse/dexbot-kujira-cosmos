375edd8e4a5a5d0cca38f1b245c5306980671ee7
Lutwidse <lutwidse@m0n4.com>
2023-01-20 14:17:52 +0900

    add comment

--- src/config.ts
+++ src/config.ts
@@ -3,17 +3,25 @@ export const MNEMONIC = '...';
 
 export const DENOM_AMOUNT = 1000000;
 
-export const ATOM_USK_CONTRACT =
+// FIN Contract
+export const FIN_ATOM_USK_CONTRACT =
   'kujira1yum4v0v5l92jkxn8xpn9mjg7wuldk784ctg424ue8gqvdp88qzlqr2qp2j';
-export const ATOM_DENOM =
-  'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2';
 
+export const FIN_ATOM_axlUSDC_CONTRACT =
+  'kujira1xr3rq8yvd7qplsw5yx90ftsr2zdhg4e9z60h5duusgxpv72hud3sl8nek6';
+
+// ORCA Contract
 export const ORCA_MARKET_USK_ATOM_CONTRACT =
   'kujira1q8y46xg993cqg3xjycyw2334tepey7dmnh5jk2psutrz3fc69teskctgfc';
 
+// Kujira Denom
 export const USK_DENOM =
   'factory/kujira1qk00h5atutpsv900x202pxx42npjr9thg58dnqpa72f2p7m2luase444a7/uusk';
 
+export const ATOM_DENOM =
+  'ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2';
+
+// SETTING
 export const PREMIUM = 7;
 
 export const BID_MAX = 5;
